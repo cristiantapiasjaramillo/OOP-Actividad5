8c7a3af4c6bc8aec15de508c1bd2a235eeec752c
cristiantapiasjaramillo <cristiantapias75@gmail.com>
2025-02-09 21:48:38 -0500

    Add files via upload

---
+++ Figuras/src/main/java/com/mycompany/figuras/Figuras.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.figuras;
+
+/**
+ *
+ * @author crist
+ */
+public class Figuras {
+
+public static void main(String[] args) {
+VentanaPrincipal miVentanaPrincipal; 
+miVentanaPrincipal= new VentanaPrincipal(); 
+miVentanaPrincipal.setVisible(true); 
+miVentanaPrincipal.setResizable(false);
+}
+}
+
